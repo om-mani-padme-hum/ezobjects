c880799c4c6725282b427614ae40f82624b7b484
Rich Lowe <rich.e.lowe@ge.com>
2018-05-09 14:20:16 -0500

    Enabled dynamic extension after painstaking effort

--- index.js
+++ index.js
@@ -12,135 +12,136 @@ module.exports = (table) => {
     parent = global;
   
   /** Create new class on global scope */
-  parent[table.className] = class {
+  parent[table.className] = class extends (table.extends || Object) {
     /** Constructor for new object. */
     constructor(data = {}) {
+      super(data);
+
+      this.init = function (data = {}) {
+        /** Loop through each field in the table */
+        table.fields.forEach((col) => {
+          /** Initialize 'int' and 'float' types to zero */
+          if ( col.type == 'int' || col.type == 'float' )
+            this[col.name](data[col.name] || col.default || 0);
+
+          /** Initialize 'string' types to empty */
+          else if ( col.type == 'string' )
+            this[col.name](data[col.name] || col.default || '');
+
+          /** Initialize 'Array' types to empty */
+          else if ( col.type == 'Array' )
+            this[col.name](data[col.name] || col.default || []);
+
+          /** Initialize all other types to null */
+          else
+            this[col.name](data[col.name] || col.default || null);
+        });
+      }
+
       /** Loop through each field in the table */
       table.fields.forEach((col) => {
         /** For 'int' type fields */
         if ( col.type == 'int' ) {
-          this[col.name] = (arg) => { 
+          this[col.name] = function (arg) { 
             /** Getter */
             if ( arg === undefined ) 
               return this[`_${col.name}`]; 
-            
+
             /** Setter */
             else if ( typeof arg == 'number' ) 
               this[`_${col.name}`] = parseInt(arg); 
-            
+
             /** Handle type errors */
             else 
               throw new Error(`${table.className}.${col.name}(${typeof arg}): Invalid signature.`); 
-            
+
             /** Return this object for set call chaining */
             return this; 
           };
         } 
-        
+
         /** For 'float' type fields */
         else if ( col.type == 'float' ) {
-          this[col.name] = (arg) => { 
+          this[col.name] = function (arg) { 
             /** Getter */
             if ( arg === undefined ) 
               return this[`_${col.name}`]; 
-            
+
             /** Setter */
             else if ( typeof arg == 'number' ) 
               this[`_${col.name}`] = parseFloat(arg); 
-            
+
             /** Handle type errors */
             else 
               throw new Error(`${table.className}.${col.name}(${typeof arg}): Invalid signature.`); 
-            
+
             /** Return this object for set call chaining */
             return this; 
           };
         } 
-        
+
         /** For 'string' type fields */
         else if ( col.type == 'string' ) {
-          this[col.name] = (arg) => { 
+          this[col.name] = function (arg) { 
             /** Getter */
             if ( arg === undefined ) 
               return this[`_${col.name}`]; 
-            
+
             /** Setter */
             else if ( typeof arg == 'string' ) 
               this[`_${col.name}`] = arg.toString(); 
-            
+
             /** Handle type errors */
             else 
               throw new Error(`${table.className}.${col.name}(${typeof arg}): Invalid signature.`); 
-            
+
             /** Return this object for set call chaining */
             return this; 
           };
         } 
-        
+
         /** For 'Array' type fields */
         else if ( col.type == 'Array' ) {
-          this[col.name] = (arg) => { 
+          this[col.name] = function (arg) { 
             /** Getter */
             if ( arg === undefined ) 
               return this[`_${col.name}`]; 
-            
+
             /** Setter */
             else if ( typeof arg == 'object' && arg.constructor.name == col.type )
               this[`_${col.name}`] = arg; 
-            
+
             /** Handle type errors */
             else 
               throw new Error(`${table.className}.${col.name}(${typeof arg}): Invalid signature.`); 
-            
+
             /** Return this object for set call chaining */
             return this; 
           };
         }
-        
+
         /** For all other field types */
         else {
-          this[col.name] = (arg) => { 
+          this[col.name] = function (arg) { 
             /** Getter */
             if ( arg === undefined ) 
               return this[`_${col.name}`]; 
-            
+
             /** Setter */
             else if ( arg === null || ( typeof arg == 'object' && arg.constructor.name == col.type ) ) 
               this[`_${col.name}`] = arg; 
-            
+
             /** Handle type errors */
             else 
               throw new Error(`${table.className}.${col.name}(${typeof arg}): Invalid signature.`); 
-            
+
             /** Return this object for set call chaining */
             return this; 
           };
         }
       });
-      
+
       this.init(data);
-    }
-    
-    /** Initialize object with any provided `data` */
-    init(data = {}) {
-      /** Loop through each field in the table */
-      table.fields.forEach((col) => {
-        /** Initialize 'int' and 'float' types to zero */
-        if ( col.type == 'int' || col.type == 'float' )
-          this[col.name](data[col.name] || col.default || 0);
-        
-        /** Initialize 'string' types to empty */
-        else if ( col.type == 'string' )
-          this[col.name](data[col.name] || col.default || '');
-        
-        /** Initialize 'Array' types to empty */
-        else if ( col.type == 'Array' )
-          this[col.name](data[col.name] || col.default || []);
-        
-        /** Initialize all other types to null */
-        else
-          this[col.name](data[col.name] || col.default || null);
-      });
     }
   }
   
