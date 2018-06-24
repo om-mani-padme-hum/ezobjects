704b2ba8463275e1c81967b0f8ecece4902318dd
Rich <rich.fb.email@gmail.com>
2018-06-24 18:31:49 -0500

    Added installation section to README, updated patch version

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v2.11.1`);
+docket.title(`EZ Objects v2.11.2`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`, `mysql-connection.js`]);
 docket.generateDocs(`docs`);
