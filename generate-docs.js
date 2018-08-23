7d837e3256774b1de9c90a92263b96dbee980d21
Rich <rich.fb.email@gmail.com>
2018-08-23 18:27:01 -0500

    Updated version

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v5.4.1`);
+docket.title(`EZ Objects v5.4.2`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`]);
 docket.generateDocs(`docs`);
